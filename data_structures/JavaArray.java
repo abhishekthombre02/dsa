b93078b52448322b9f3ff59fc311435d30d3836e
Abhishek Thombre <abhishekthombre25@gmail.com>
2024-04-30 05:00:32 +0530

    JavaArray added

---
+++ data_structures/JavaArray.java
@@ -0,0 +1,32 @@
+
+// public class
+public class JavaArray{
+
+    public static void main(String[] args) {
+        
+        //initialise array of size 10
+        int[] arr=new int[10];
+
+        //print base address of array
+        System.out.println(arr);
+
+        //Store value in array
+        for(int i=0;i<10;i++){
+            arr[i]=i+1;
+        }
+
+        //for loop help to iterate over array using index value 
+        //condition that the index value should be in range from 0 to array.length-1
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+
+        //for-each loop help to iterate over array without using index value 
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
+    }
+}
